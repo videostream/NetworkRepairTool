ea91cb9b8f4aa1e24f527f22eaa79c5abcc75dea
Mathieu Gosbee <mail@matbee.com>
2014-11-21 09:34:21 -0500

    first commit

---
+++ VideostreamNetworkRepair/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace VideostreamNetworkRepair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Repair Firewall";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Repair Connection Profile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(278, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Repair Needed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(278, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Repair Needed";
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button3.Location = new System.Drawing.Point(270, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Done";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 199);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Videostream Repair";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ VideostreamNetworkRepair/Form1.cs
@@ -0,0 +1,96 @@
+﻿using NETWORKLIST;
+using NetFwTypeLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideostreamNetworkRepair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT && Environment.OSVersion.Version.Major == 5 && Environment.OSVersion.Version.Minor == 1)
+            {
+                // windows XP. Disable profile button.
+                button2.Hide();
+                label2.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openPort(5556, "Videostream Desktop Application");
+            this.openPort(5558, "Videostream Mobile Application");
+            label1.ForeColor = Color.Green;
+            label1.Text = "Ready";
+        }
+
+        private void openPort(int port, string name)
+        {
+            INetFwOpenPort portClass;
+            Type TportClass = Type.GetTypeFromProgID("HNetCfg.FWOpenPort");
+            portClass = (INetFwOpenPort)Activator.CreateInstance(TportClass);
+            // Set the port properties
+            portClass.Scope = NetFwTypeLib.NET_FW_SCOPE_.NET_FW_SCOPE_ALL;
+            portClass.Enabled = true;
+            portClass.Protocol = NetFwTypeLib.NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+            portClass.Name = name;
+            portClass.Port = port;
+
+            INetFwMgr icfMgr = null;
+            try
+            {
+                Type TicfMgr = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+                icfMgr = (INetFwMgr)Activator.CreateInstance(TicfMgr);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            var profile = icfMgr.LocalPolicy.CurrentProfile;
+            // Add the port to the ICF Permissions List
+            profile.GloballyOpenPorts.Add(portClass);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var manager = new NetworkListManager();
+            var connectedNetworks = manager.GetNetworks(NLM_ENUM_NETWORK.NLM_ENUM_NETWORK_CONNECTED).Cast<INetwork>();
+            foreach (var network in connectedNetworks)
+            {
+                Console.Write(network.GetName() + " ");
+                var cat = network.GetCategory();
+                if (cat == NLM_NETWORK_CATEGORY.NLM_NETWORK_CATEGORY_PRIVATE) {
+                    Console.WriteLine("[PRIVATE]");
+                }
+                else if (cat == NLM_NETWORK_CATEGORY.NLM_NETWORK_CATEGORY_PUBLIC)
+                {
+                    Console.WriteLine("[PUBLIC]");
+                    network.SetCategory(NLM_NETWORK_CATEGORY.NLM_NETWORK_CATEGORY_PRIVATE);
+                }
+                else if (cat == NLM_NETWORK_CATEGORY.NLM_NETWORK_CATEGORY_DOMAIN_AUTHENTICATED)
+                    Console.WriteLine("[DOMAIN]");
+            }
+            label2.ForeColor = Color.Green;
+            label2.Text = "Ready";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            
+        }
+    }
+}
